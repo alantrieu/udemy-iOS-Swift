5aa2b33cec044ae43ed3fbcc95e567c4dc5ff430
Alan Trieu <alantrieu@192-168-1-111.tpgi.com.au>
2022-08-29 15:35:05 +1000

    practicing with SwiftUI

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Alan Trieu on 29/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
