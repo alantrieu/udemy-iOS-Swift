6e24ca565843707d936daa084a9b302cca34daa2
Alan Trieu <alantrieu@192-168-1-111.tpgi.com.au>
2022-09-01 23:37:15 +1000

    made compatible with CoreData; SQLite database

---
+++ Todoey-Realm-iOS13-master/Todoey/Models/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Alan Trieu on 1/9/2022.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Todoey-Realm-iOS13-master/Todoey/Models/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Alan Trieu on 1/9/2022.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
